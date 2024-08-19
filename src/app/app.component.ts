7d443bacc5e500b6afdde76123636f5aeb9fa3fb
JOAN <joanandres10@gmail.com>
2024-08-19 16:20:41 -0500

    pokemon api con señales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./layout/footer/footer.component";
+import { HeaderComponent } from "./layout/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+
 })
 export class AppComponent {
   title = 'PokApiSignal';
