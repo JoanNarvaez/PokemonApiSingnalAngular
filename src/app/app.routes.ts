40a47aabf9e95b0b169b9619a3ea73e31fe0d5c0
JOAN <joanandres10@gmail.com>
2024-08-21 13:34:59 -0500

    se acomoda routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import PokeApiComponent from './poke-api/poke-api.component';
 
 export const routes: Routes = [{
-    path: 'a',
+    path: 'poke-api-signal',
     // loadComponent: () =>
     //     import('./poke-api/poke-api.component')
     component:PokeApiComponent
