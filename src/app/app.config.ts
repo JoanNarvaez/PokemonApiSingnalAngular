0f9b085a5b2b06c2f50b8e81cd9afc354d1ff29d
JOAN <joanandres10@gmail.com>
2024-08-21 14:14:49 -0500

    se realiza ajustes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,9 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-// import ROUTES  from './app-routings';
 
-// export const appConfig: ApplicationConfig = {
-//   providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync()]
-// };
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import PokeApiComponent from './poke-api/poke-api.component';
 
 export const routes: Routes = [{
-    path: 'poke-api-signal',
-    // loadComponent: () =>
-    //     import('./poke-api/poke-api.component')
-    component:PokeApiComponent
+    path: "pokemon",
+    loadComponent: () =>
+        import('./poke-api/poke-api.component')
+    // component:PokeApiComponent
 }];
